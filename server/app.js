0a8ae4279ef191bdf10a47b74689b272d09ace6d
Jie Burillo <benjieburillo10@gmail.com>
2023-07-05 23:17:20 +0800

    update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
+require('dotenv').config()
 const express = require('express')
+const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('Hello World!')
-})
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1/crud', require('./routes/user'))
+app.use(notFound)
+async function start() {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(process.env.PORT, () => {
+            console.log('Server is up on port', process.env.PORT)
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-})
+start()
