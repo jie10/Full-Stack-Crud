4b3bd7b68f9a339e9f43c9c99ffaa4c3a3f32c49
Jie Burillo <benjieburillo10@gmail.com>
2023-07-08 18:58:51 +0800

    import tailwindcss

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import 'tailwindcss/tailwind.css'
 const app = createApp(App)
 
 app.use(createPinia())
