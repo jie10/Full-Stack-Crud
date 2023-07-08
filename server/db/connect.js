b1b11887e8d9641e2bfcfd9dfaf2f1b9de7e1df3
Jie Burillo <benjieburillo10@gmail.com>
2023-07-08 19:13:13 +0800

    add comment for connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose')
+// Importing the Mongoose library
+const mongoose = require("mongoose")
 
+// Function to connect to the MongoDB database
 function connectDB(url) {
-    try {
-        mongoose.connect(url)
-        console.log('MongoDB Connected...')
-    } catch (error) {
-        console.error(error)
-    }
+   try {
+      // Connecting to the MongoDB database using the provided URL
+      mongoose.connect(url)
+      console.log("MongoDB Connected...")
+   } catch (error) {
+      // Handling any errors that occur during the connection process
+      console.error(error)
+   }
 }
 
+// Exporting the connectDB function to be used in other parts of the codebase
 module.exports = connectDB
